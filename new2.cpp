6a4cd68114ee8dbac1027c5035054bc2bf077440
tsgibson <tsgibson@ucdavis.edu>
2014-07-11 15:33:05 -0700

    Fixed new2.cpp code

--- new2.cpp
+++ new2.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     printf("Hello World!\n");
-    cout << "Hellow World, again!\n";
+    std::cout << "Hellow World, again!\n";
 
     return 0;
 }
